e06a97637ad7973cd5d817f17a51514d804bbbd0
landim9 <landimr9des@gmail.com>
2023-11-15 16:37:56 -0300

    senai

---
+++ assets/pages/Password Generator/assets/script/main.js
@@ -0,0 +1,26 @@
+// function generatePassword(length) {
+//   let charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+//   let password = "";
+//   for (let i = 0; i < length; i++) {
+//     password += charset.charAt(Math.floor(Math.random() * charset.length));
+//   }
+
+//   console.log(password)
+
+
+//   return password;
+
+// }
+
+// generatePassword(10)
+
+function generatePassword() {
+  const length = document.getElementById('length').value;
+  const charset = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$';
+  let password = '';
+  for (let i = 0, n = charset.length; i < length; ++i) {
+    password += charset.charAt(Math.floor(Math.random() * n));
+  }
+  document.getElementById('password').textContent = password;
+}
+
